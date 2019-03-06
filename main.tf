127df9723f79a806f082ffad452978770d858915
Bennett, Thomas <thomas.bennett@sky.uk>
2019-03-06 21:39:04 +0000

    Add terraform outputs

--- main.tf
+++ main.tf
@@ -31,3 +31,15 @@ resource "google_container_cluster" "primary" {
     ]
   }
 }
+
+output "client_certificate" {
+  value = "${google_container_cluster.primary.master_auth.0.client_certificate}"
+}
+
+output "client_key" {
+  value = "${google_container_cluster.primary.master_auth.0.client_key}"
+}
+
+output "cluster_ca_certificate" {
+  value = "${google_container_cluster.primary.master_auth.0.cluster_ca_certificate}"
+}
