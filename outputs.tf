04a3ba369a61c6fbdab25f2a385f114e149c7944
Bennett, Thomas <thomas.bennett@sky.uk>
2019-03-11 15:57:04 +0000

    output service account

--- outputs.tf
+++ outputs.tf
@@ -17,3 +17,7 @@ output "client_key" {
 output "cluster_ca_certificate" {
   value = "${google_container_cluster.primary.master_auth.0.cluster_ca_certificate}"
 }
+
+output "helm_service_account" {
+  value = "${kubernetes_service_account.tiller.metadata.0.name}"
+}
