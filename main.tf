6841f41c58223f35926acc68ae8078d918247ca9
Bennett, Thomas <thomas.bennett@sky.uk>
2019-03-11 14:18:18 +0000

    Move outputs into separate file

--- main.tf
+++ main.tf
@@ -25,23 +25,3 @@ resource "google_container_cluster" "primary" {
     ]
   }
 }
-
-output "token" {
-  value = "${data.google_client_config.default.access_token}"
-}
-
-output "host" {
-  value = "${google_container_cluster.primary.endpoint}"
-}
-
-output "client_certificate" {
-  value = "${google_container_cluster.primary.master_auth.0.client_certificate}"
-}
-
-output "client_key" {
-  value = "${google_container_cluster.primary.master_auth.0.client_key}"
-}
-
-output "cluster_ca_certificate" {
-  value = "${google_container_cluster.primary.master_auth.0.cluster_ca_certificate}"
-}
