eb18cf64372fe30740e1c3fe82b02a36f8d122b2
Thomas Bennett <tombennett86@hotmail.co.uk>
2019-03-11 22:14:40 +0000

    moved monitoring

--- helm/main.tf
+++ helm/main.tf
@@ -16,6 +16,6 @@ resource "helm_release" "prometheus_operator" {
   namespace = "monitoring"
 
   values = [
-    "${file("${path.root}/monitoring/prometheus/values.yml")}",
+    "${file("${path.module}/monitoring/prometheus/values.yml")}",
   ]
 }
