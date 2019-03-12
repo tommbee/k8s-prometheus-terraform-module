2040c206f31eb29c82bf60a408491ab9c6bc203d
Bennett, Thomas <thomas.bennett@sky.uk>
2019-03-12 15:01:05 +0000

    test

--- k8s/main.tf
+++ k8s/main.tf
@@ -8,36 +8,37 @@ provider "kubernetes" {
   load_config_file = false
 }
 
-resource "kubernetes_service_account" "tiller" {
-  metadata {
-    name      = "tiller"
-    namespace = "kube-system"
-  }
-  #automount_service_account_token = true
-}
+# resource "kubernetes_service_account" "tiller" {
+#   metadata {
+#     name      = "tiller"
+#     namespace = "kube-system"
+#   }
+#   #automount_service_account_token = true
+# }
 
-resource "kubernetes_cluster_role_binding" "tiller" {
-  metadata {
-    name = "tiller"
-  }
+# resource "kubernetes_cluster_role_binding" "tiller" {
+#   metadata {
+#     name = "tiller"
+#   }
 
-  role_ref {
-    api_group = "rbac.authorization.k8s.io"
-    kind      = "ClusterRole"
-    name      = "cluster-admin"
-  }
+#   role_ref {
+#     api_group = "rbac.authorization.k8s.io"
+#     kind      = "ClusterRole"
+#     name      = "cluster-admin"
+#   }
 
-  subject {
-    api_group = ""
-    kind      = "ServiceAccount"
-    name      = "tiller"
-    namespace = "kube-system"
-  }
-}
+#   subject {
+#     api_group = ""
+#     kind      = "ServiceAccount"
+#     name      = "tiller"
+#     namespace = "kube-system"
+#   }
+# }
 
 provider "helm" {
   version = "~> 0.6"
-  service_account = "${kubernetes_service_account.tiller.metadata.0.name}"
+  install_tiller = "true"
+  service_account = "tiller"
 
   kubernetes {
     #client_certificate     = "${var.client_certificate}"
@@ -45,6 +46,7 @@ provider "helm" {
     cluster_ca_certificate = "${var.cluster_ca_certificate}"
     host                   = "${var.host}"
     token                  = "${var.token}"
+    config_context = "tiller"
   }
 }
 
@@ -52,7 +54,6 @@ resource "helm_release" "prometheus_operator" {
   name  = "monitoring"
   chart = "stable/prometheus-operator"
   #namespace = "monitoring"
-  depends_on = ["kubernetes_service_account.tiller"]
 
   values = [
     "${file("${path.module}/monitoring/prometheus/values.yml")}",
