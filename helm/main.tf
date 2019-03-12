95caf1798b82cec17d2c551c6f6d97d826f68375
Bennett, Thomas <thomas.bennett@sky.uk>
2019-03-12 22:16:43 +0000

    cheese

--- helm/main.tf
+++ helm/main.tf
@@ -11,12 +11,6 @@ provider "helm" {
   }
 }
 
-resource "null_resource" "helm_init" {
-  provisioner "local-exec" {
-    command = "helm init --client-only"
-  }
-}
-
 resource "null_resource" "depends_on_hack" {
   triggers {
     version = "${timestamp()}"
@@ -36,7 +30,6 @@ resource "helm_release" "prometheus_operator" {
 
   depends_on = [
       "null_resource.depends_on_hack",
-      "null_resource.helm_init",
   ]
 
   values = [
