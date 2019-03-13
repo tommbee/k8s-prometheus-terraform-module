338d44f4b9f7a99d8cbdb73e8f867c2e0a7edefc
Bennett, Thomas <thomas.bennett@sky.uk>
2019-03-13 22:51:49 +0000

    te

--- helm/outputs.tf
+++ helm/outputs.tf
@@ -1,7 +1,7 @@
 output "helm_service_account" {
-  value = "${null_resource.helm_init.connection.service_account}"
+  value = "${null_resource.helm_init.connection.0.service_account}"
 }
 
 output "helm_namespace" {
-  value = "${null_resource.helm_init.connection.namespace}"
+  value = "${null_resource.helm_init.connection.0.namespace}"
 }
