a86e57a0c9f27747368bee5cd5ac0c03e3ca0a57
Bennett, Thomas <thomas.bennett@sky.uk>
2019-03-13 09:27:17 +0000

    test

--- helm/main.tf
+++ helm/main.tf
@@ -22,21 +22,20 @@ resource "null_resource" "depends_on_hack" {
   }
 }
 
-# resource "helm_repository" "coreos" {
-#   name = "coreos"
-#   url  = "https://s3-eu-west-1.amazonaws.com/coreos-charts/stable/"
-# }
+resource "helm_repository" "coreos" {
+  name = "coreos"
+  url  = "https://s3-eu-west-1.amazonaws.com/coreos-charts/stable/"
+}
 
 resource "helm_release" "prometheus_operator" {
   name       = "prometheus-operator"
-  #repository = "${helm_repository.coreos.metadata.0.name}"
-  repository = "https://s3-eu-west-1.amazonaws.com/coreos-charts/stable/"
+  repository = "${helm_repository.coreos.metadata.0.name}"
   chart      = "prometheus-operator"
   namespace  = "monitoring"
 
   depends_on = [
       "null_resource.depends_on_hack",
-      #"helm_repository.coreos",
+      "helm_repository.coreos",
   ]
 
   values = [
