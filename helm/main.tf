6a200c4781d7c1ed84d959b0d2256f52cedde213
Bennett, Thomas <thomas.bennett@sky.uk>
2019-03-13 13:20:42 +0000

    new helm chart for prometheus

--- helm/main.tf
+++ helm/main.tf
@@ -23,20 +23,22 @@ resource "null_resource" "depends_on_hack" {
   }
 }
 
-resource "helm_repository" "coreos" {
-  name = "coreos"
-  url  = "https://s3-eu-west-1.amazonaws.com/coreos-charts/stable/"
-}
+# resource "helm_repository" "coreos" {
+#   name = "coreos"
+#   url  = "https://s3-eu-west-1.amazonaws.com/coreos-charts/stable/"
+# }
 
 resource "helm_release" "prometheus_operator" {
   name       = "prometheus-operator"
   repository = "${helm_repository.coreos.metadata.0.name}"
-  chart      = "prometheus-operator"
+  #repository = "${helm_repository.coreos.metadata.0.name}"
+  #chart      = "prometheus-operator"
+  chart      = "stable/prometheus-operator"
   namespace  = "monitoring"
 
   depends_on = [
       "null_resource.depends_on_hack",
-      "helm_repository.coreos",
+      #"helm_repository.coreos",
   ]
 
   values = [
