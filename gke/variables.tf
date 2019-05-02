8b1f89cac45a95cf6d01925b429a4cd70728bae7
Bennett, Thomas <thomas.bennett@sky.uk>
2019-05-02 21:50:22 +0100

    Remove default var valies

--- gke/variables.tf
+++ gke/variables.tf
@@ -1,12 +1,7 @@
-variable "projet_name" {
-    default = "temporal-parser-229715"
-}
-variable "region" {
-  default = "europe-west1-c"
-}
+variable "projet_name" {}
 
-variable "config_file" {
-  default="article-app-google-account-creds.json"
-}
+variable "region" {}
+
+variable "config_file" {}
 
 variable "cluster_name" {}
