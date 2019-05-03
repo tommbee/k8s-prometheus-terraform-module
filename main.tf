8e5c3c93afd6c5ad92acc72a86e011b7e73b314e
Bennett, Thomas <thomas.bennett@sky.uk>
2019-05-03 14:45:25 +0100

    add provider for bucket upload

--- main.tf
+++ main.tf
@@ -14,6 +14,26 @@ module "k8s" {
   token =  "${module.gke_cluster.token}"
 }
 
+provider "google" {
+  credentials = "${file("${var.config_file}")}"
+}
+
+data "archive_file" "kubeconfig" {
+  type        = "zip"
+  output_path = "kubeconfig.zip"
+
+  source {
+    content  = "${module.gke_cluster.kubeconfig}"
+    filename = "kubeconfig"
+  }
+}
+
+resource "google_storage_bucket_object" "kubeconfigzip" {
+  name   = "kubeconfig.zip"
+  source = "${data.archive_file.kubeconfig.output_path}"
+  bucket = "${var.gcs_bucket}"
+}
+
 # module "helm" {
 #   source = "helm"
 
